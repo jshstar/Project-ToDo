3a6a1145e4e1e88752b0377770fcd74d79ded702
jshstar <jshstar97@gmail.com>
2023-12-02 17:34:49 +0900

    Feat(CommentServiceTest update)

--- src/main/java/com/sparta/project_todo/dto/CommentRequestDto.java
+++ src/main/java/com/sparta/project_todo/dto/CommentRequestDto.java
@@ -2,9 +2,11 @@ package com.sparta.project_todo.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public class CommentRequestDto {
     @NotBlank(message = "댓글을 작성해 주세요")
-    private String comment; // 댓글 내용
+    private final String comment; // 댓글 내용
 }
--- src/test/java/com/sparta/project_todo/service/CommentServiceTest.java
+++ src/test/java/com/sparta/project_todo/service/CommentServiceTest.java
@@ -1,21 +1,80 @@
 package com.sparta.project_todo.service;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
 
+import java.util.Optional;
+
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 
+import com.sparta.project_todo.dto.CommentRequestDto;
+import com.sparta.project_todo.dto.CommentResponseDto;
+import com.sparta.project_todo.dto.ToDoRequestDto;
+import com.sparta.project_todo.entity.Comment;
+import com.sparta.project_todo.entity.ToDoCard;
+import com.sparta.project_todo.entity.User;
+import com.sparta.project_todo.entity.UserRoleEnum;
+import com.sparta.project_todo.repository.CommentRepository;
+import com.sparta.project_todo.repository.ToDoRepository;
 
 class CommentServiceTest {
 
-	@Test
-	void createComment() {
+	@Mock
+	CommentRepository commentRepository;
+	@Mock
+	ToDoRepository toDoRepository;
+
+	@InjectMocks
+	ToDoService toDoService;
+
+	@InjectMocks
+	CommentService commentService;
+
+	public User testcreateUser(String num){
+		return new User("username"+ num,"123456789", UserRoleEnum.USER);
+	}
+	public ToDoCard testCreateToDoCard (String num, User user){
+		ToDoRequestDto requestDto = new ToDoRequestDto("testTitle" + num, "testContents" +num);
+		ToDoCard toDoCard = new ToDoCard(requestDto, user);
+		if(Integer.parseInt(num)%2 ==1){
+			toDoCard.hiddenFlag(true);
+		}
+		else {
+			toDoCard.hiddenFlag(false);
+		}
+		return toDoCard;
 	}
 
-	@Test
-	void updateComment() {
+	public Comment testcreateComment(String comment, User user, ToDoCard toDoCard) {
+		return new Comment(comment,user, toDoCard);
 	}
 
-	@Test
-	void deleteComment() {
+
+
+	@Nested
+	class 댓글_생성테스트{
+		@Test
+		void 댓글생성_테스트() throws IllegalAccessException {
+			//given
+			User user1 = testcreateUser("1");
+			ToDoCard toDoCard = testCreateToDoCard("1", user1);
+			User user2 = testcreateUser("2");
+			CommentRequestDto commentRequestDto = new CommentRequestDto("comment");
+			Comment comment = testcreateComment(commentRequestDto.getComment(),user2, toDoCard);
+			given(toDoService.findCard(1L)).willReturn(Optional.of(toDoCard));
+			given(commentRepository.save(comment)).willReturn(comment);
+
+			//when
+			CommentResponseDto result = commentService.createComment(1L,commentRequestDto, user2);
+
+			//then
+			assertEquals("comment", result.getComment());
+
+		}
+
+
 	}
 }
